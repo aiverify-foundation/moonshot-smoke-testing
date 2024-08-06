4ee1ea8d7d527413d2656a26275f1b6daf669cbd
Jackson Boey <jackson_boey@imda.gov.sg>
2024-08-06 16:32:45 +0800

    update testing test case test_util_page_view_prompt-templates

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,10 +64,10 @@ export default defineConfig({
       },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',
---
+++ tests/util-page-testing.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { main_page_startup } from './common';
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Test moonshot - init page
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Test case for verifying the startup page elements.
+ * It navigates to the startup page and checks for the visibility of various elements.
+ *
+ * @param {import('@playwright/test').Page} page - The Playwright page object.
+ */
+test.use({
+  viewport: {
+    height: 1080,
+    width: 1920
+  }
+});
+test('test_util_page_view_prompt-templates', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await main_page_startup(page);
+  const submitButton = page.locator('#navContainer').getByRole('link').nth(4)
+  await expect(submitButton).toBeVisible();
+  await submitButton.click();
+  // Assertion for redirecting to right page
+  await expect(page.getByRole('heading', { name: 'moonshot utilities' })).toBeVisible();
+  await page.getByRole('button', { name: 'View Prompt Templates' }).click();
+  await expect(page.locator('header').filter({ hasText: 'Prompt Templates' })).toBeVisible();
+  await expect(page.locator('li').filter({ hasText: 'tamil-templatenewsclassificationThis template is used for Tamil News' })).toBeVisible();
+});

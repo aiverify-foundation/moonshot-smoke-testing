7a1db92cadafc6e707c9150f89342ff795160924
Jackson Boey <jackson_boey@imda.gov.sg>
2024-08-08 09:35:31 +0800

    Update test cases for util feature

--- tests/util-page-testing.spec.ts
+++ tests/util-page-testing.spec.ts
@@ -16,15 +16,31 @@ test.use({
     width: 1920
   }
 });
-test('test_util_page_view_prompt-templates', async ({ page }) => {
+
+async function open_home_page_select_util_tab(page) {
+  // Check if the left panel is correct and visible by looking for unique text within it (has the Added on)
   await page.goto('http://localhost:3000/');
   await main_page_startup(page);
-  const submitButton = page.locator('#navContainer').getByRole('link').nth(4)
-  await expect(submitButton).toBeVisible();
-   await submitButton.click();
+  const linkTabButton = page.locator('#navContainer').getByRole('link').nth(4)
+  await expect(linkTabButton).toBeVisible();
+   await linkTabButton.click();
   // Assertion for redirecting to right page
   await expect(page.getByRole('heading', { name: 'moonshot utilities' })).toBeVisible();
+}
+
+
+test('test_util_page_view_prompt-templates', async ({ page }) => {
+  await open_home_page_select_util_tab(page)
   await page.getByRole('button', { name: 'View Prompt Templates' }).click();
   await expect(page.locator('header').filter({ hasText: 'Prompt Templates' })).toBeVisible();
   await expect(page.locator('li').filter({ hasText: 'tamil-templatenewsclassificationThis template is used for Tamil News' })).toBeVisible();
+});
+
+test('test_util_page_view_context-strategies', async ({ page }) => {
+  await open_home_page_select_util_tab(page)
+  await expect(page.getByRole('heading', { name: 'moonshot utilities' })).toBeVisible();
+  await page.getByRole('button', { name: 'View Context Strategies' }).click();
+  await expect(page.getByRole('heading', { name: 'Context Strategies' })).toBeVisible();
+  await expect(page.locator('h4')).toContainText('Add Previous Prompt');
+  await expect(page.locator('body')).toContainText('This is a sample context strategy that adds in previous prompts to the current prompt. [Default: 5]');
 });
